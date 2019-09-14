63707532688d2b05a75517d363ccd03079f4e1ad
Julia Kreutzer <juliakreutzer1@aol.com>
2019-09-15 00:40:59 +0200

    displaying image and co2

--- app/app.py
+++ app/app.py
@@ -1,17 +1,18 @@
 import io
 import json
+import os
 
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
 from flask import Flask, jsonify, request, render_template, redirect
-
+from .utils import load_classes
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 imagenet_class_index = json.load(open('app/imagenet_class_index.json'))
 model = models.densenet121(pretrained=True)
-#models.squeezenet1_1(pretrained=True) 
-#models.densenet121(pretrained=True)
+class_dict = load_classes('app/static/garbage/')
 model.eval()
 
 
@@ -34,17 +35,54 @@ def get_prediction(image_bytes):
     return imagenet_class_index[predicted_idx]
 
 
+def garbage_class(class_name):
+    """
+    Classify an ImageNet category into 4 garbage classes and get CO2 mass.
+    Example 'plastic_bag' -> 'recycle', '0.4'
+
+    :param class_name: ImageNet class name.
+    :return:
+        Garbage class, one of "compost", "recycle", "general", "hazardous".
+        CO2 mass, also string.
+    """
+    normalized = class_name.split(',')[0].lower()
+    for c, v in class_dict.items():
+        for o, co in v:
+            if normalized == o:
+                return c, co
+    return "No garbage, detected a {}".format(normalized.replace('_', ' ')), "?"
+
+
 @app.route('/', methods=['POST', 'GET'])
 def predict():
+    # TODO ugly handling of temp files.
+    temp_file = "app/static/temp/upload.jpg"
     if request.method == 'POST':
         file = request.files['file']
         img_bytes = file.read()
+        with open(temp_file, mode="wb") as jpg:
+            jpg.write(img_bytes)
         class_id, class_name = get_prediction(image_bytes=img_bytes)
-        return render_template('result.html',class_id=class_id,
-                               class_name=class_name)
-        # jsonify({'class_name': class_name}) 
-        # jsonify({'class_id': class_id, 'class_name': class_name})
+        garbage_type, co = garbage_class(class_name)
+        return render_template('result.html',
+                               class_name=class_name,
+                               garbage_type=garbage_type,
+                               co=co)
+    if os.path.exists(temp_file):
+        os.remove(temp_file)  # this deletes the file
+
     return render_template('index.html')
+
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
 
 if __name__ == '__main__':
     app.run()
---
+++ app/utils.py
@@ -0,0 +1,20 @@
+import os
+
+def load_classes(dir):
+    """
+    Load the garbage classes from the given directory.
+    The file name corresponds to the class.
+    The file contains one object and its CO2 mass per line, separated by tab.
+
+    :param dir: directory containing garbage classes
+    :return: dictionary mapping garbage classes to objects and their CO2 mass.
+    """
+    class_dict = {}
+    for file_name in os.listdir(dir):
+        class_name = file_name.split(".txt")[0]
+        class_dict[class_name] = []
+        with open(dir+'/'+file_name, 'r') as f:
+            for line in f:
+                object, co = line.strip().split()
+                class_dict[class_name].append((object, co))
+    return class_dict
